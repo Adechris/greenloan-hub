87bae1a01cc518e6920a7a5c9b3d038481b5d832
Adeyinka Chris <oladokun.chris@yahoo.com>
2026-05-14 17:02:11 +0100

    Cleanup: remove old src/ directory files

--- src/lib/format.ts
+++
@@ -1,9 +0,0 @@
-export const naira = (n: number) =>
-  new Intl.NumberFormat("en-NG", {
-    style: "currency",
-    currency: "NGN",
-    maximumFractionDigits: 0,
-  }).format(n);
-
-export const formatDate = (d: string | Date) =>
-  new Date(d).toLocaleDateString("en-NG", { year: "numeric", month: "short", day: "numeric" });
